0b6536a5bd702366d52b82de7fed1847c3e5a2ce
AlexSelby1 <62037865+AlexSelby1@users.noreply.github.com>
2020-03-22 17:35:00 +0000

    Add files via upload

---
+++ MorraPart2App.java
@@ -0,0 +1,45 @@
+/*
+MorraPart2App.java
+Group F
+20 3 2020
+*/
+
+import javax.swing.*;
+public class MorraPart2App{
+	public static void main(String args[]){
+
+		//variable
+		String side;//input
+		String message;//output
+		int userNumber;//input
+		int sumPMarks=0;
+		int sumCMarks=0;
+
+		//objects
+		MorraPart2 myMorraPart2;
+		myMorraPart2=new MorraPart2();
+
+		do{
+			//inputs
+			side=JOptionPane.showInputDialog(null, "To play the game, please choose between Even or Odd. Type stop to end the game: ");
+				if(!side.equals("stop") && sumCMarks<12 && sumPMarks<12){
+					myMorraPart2.setSide(side);
+					userNumber=Integer.parseInt(JOptionPane.showInputDialog(null, "Please, choose between 1 to 10: "));
+					myMorraPart2.setUserNumber(userNumber);
+
+					//process
+					myMorraPart2.compute();
+
+					//output
+					message=myMorraPart2.getMessage();
+					sumPMarks=myMorraPart2.getSumPMarks();
+					sumCMarks=myMorraPart2.getSumCMarks();
+					JOptionPane.showMessageDialog(null, message);
+					JOptionPane.showMessageDialog(null, "The atual result is Player: "+sumPMarks+" and the Computer: "+sumCMarks+".");
+			}
+		}
+		while(!side.equals("stop") && sumCMarks<12 && sumPMarks<12);
+		JOptionPane.showMessageDialog(null, "The final result is Player: "+sumPMarks+" and Computer: "+sumCMarks+".");
+
+	}
+}
