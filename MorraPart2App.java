81af1faa6ebdab4857a514b32018473e3f47a795
AlexSelby1 <62037865+AlexSelby1@users.noreply.github.com>
2020-03-24 20:14:37 +0000

    Update MorraPart2App.java

--- MorraPart2App.java
+++ MorraPart2App.java
@@ -14,6 +14,7 @@ public class MorraPart2App{
 		int userNumber;//input
 		int sumPMarks=0;
 		int sumCMarks=0;
+		String replay; //added to play again
 
 		//objects
 		MorraPart2 myMorraPart2;
@@ -32,14 +33,23 @@ public class MorraPart2App{
 
 					//output
 					message=myMorraPart2.getMessage();
+					randNumber=myMorraPart2.getRandNumber();
 					sumPMarks=myMorraPart2.getSumPMarks();
 					sumCMarks=myMorraPart2.getSumCMarks();
 					JOptionPane.showMessageDialog(null, message);
+					JOptionPane.showMessageDialog(null," The computer chose "+randNumber); //have to show what the computer chose
 					JOptionPane.showMessageDialog(null, "The atual result is Player: "+sumPMarks+" and the Computer: "+sumCMarks+".");
 			}
 		}
 		while(!side.equals("stop") && sumCMarks<12 && sumPMarks<12);
 		JOptionPane.showMessageDialog(null, "The final result is Player: "+sumPMarks+" and Computer: "+sumCMarks+".");
+				replay=JOptionPane.showInputDialog("Do you want to play again?");
+		myMorraPart2.setReplay(replay);
+			if(replay.equalsIgnoreCase("yes")){
+			main(args); // replays the game
+		} else{
+			JOptionPane.showMessageDialog(null,"Thank you");
+		}
 
 	}
 }
