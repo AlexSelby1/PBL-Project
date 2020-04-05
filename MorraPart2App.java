302b4e3834ee7599e6a8ee28b1856deb4625030f
peguilherme <62622312+peguilherme@users.noreply.github.com>
2020-04-05 14:01:44 +0100

    App class redeveloped
    The replay function was  restarting the game, than it conflicted with the arrays/score

--- MorraPart2App.java
+++ MorraPart2App.java
@@ -6,64 +6,26 @@ Group F
 
 import javax.swing.*;
 public class MorraPart2App{
+
 	public static void main(String args[]){
 
+		int con = 0;
+
+		MorraPart2Play myMorraPart2Play;
+		myMorraPart2Play=new MorraPart2Play();
+
 		//variable
-		String side;//input
-		String message;//output
-		int userNumber;//input
-		int sumPMarks=0;
-		int sumCMarks=0;
-		int randNumber=0;
-		String replay; //added to play again
-		String winnerMessage;
+		String replay= myMorraPart2Play.replay; //added to play again
+		String winnerMessage= myMorraPart2Play.winnerMessage;
 
 		//objects
 		MorraPart2 myMorraPart2;
 		myMorraPart2=new MorraPart2();
 
-		do{
-			//inputs
-			side=JOptionPane.showInputDialog(null, "To play the game, please choose between Even or Odd.\n\nType stop to end the game: \n\n");
-				if(!side.equalsIgnoreCase("stop") && sumCMarks<12 && sumPMarks<12){
-					if(side.equalsIgnoreCase("even") || side.equalsIgnoreCase("odd")){
-						myMorraPart2.setSide(side);
-						userNumber=Integer.parseInt(JOptionPane.showInputDialog(null, "Please, choose between 1 to 10: "));
-						if(userNumber>=1 && userNumber<=10){
-							myMorraPart2.setUserNumber(userNumber);
+		//Call Function play on Class MorraPart2Play to play the game
+		while(con==0) {
+			con = myMorraPart2Play.play();
+		}
 
-							//process
-							myMorraPart2.compute();
-
-							//output
-							message=myMorraPart2.getMessage();
-							randNumber=myMorraPart2.getRandNumber();
-							sumPMarks=myMorraPart2.getSumPMarks();
-							sumCMarks=myMorraPart2.getSumCMarks();
-							JOptionPane.showMessageDialog(null, message);
-							JOptionPane.showMessageDialog(null,"The computer chose: "+randNumber+"\n\nThis round result:"+"\nPlayer: "+sumPMarks+"\nComputer: "+sumCMarks); //have to show what the computer chose
-						}
-						else{
-							JOptionPane.showMessageDialog(null, "Ivalid value, start again.");
-						}
-					}
-					else{
-						JOptionPane.showMessageDialog(null, "Invalid value, start again.");
-					}
-			}
-		}
-		while(!side.equalsIgnoreCase("stop") && sumCMarks<12 && sumPMarks<12);
-		winnerMessage=myMorraPart2.getWinnerMessage();
-		winnerMessage += "\n";
-		winnerMessage += myMorraPart2.getHist();
-		JOptionPane.showMessageDialog(null, winnerMessage);
-		replay=JOptionPane.showInputDialog("Do you want to play again?");
-		myMorraPart2.setReplay(replay);
-		if(replay.equalsIgnoreCase("yes")){
-		main(args); // replays the game
-		}
-		else{
-			JOptionPane.showMessageDialog(null,"Thank you!");
-		}
 	}
 }
