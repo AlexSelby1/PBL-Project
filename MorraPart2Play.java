707db87e154bb561135f794d3b9445c456b22a9f
AlexSelby1 <62037865+AlexSelby1@users.noreply.github.com>
2020-04-07 09:10:32 +0100

    Adding some notes

--- MorraPart2Play.java
+++ MorraPart2Play.java
@@ -4,30 +4,29 @@ Group F
 03 4 2020
 */
 
-import java.util.*;
-import javax.swing.*;
+import javax.swing.*;//bring in JOptionPane
 public class MorraPart2Play{
 
 	//variable
-		String side;//input
+		String side;//input - choose side
+		int userNumber;//input - choose number
+		int randNumber;//process - random number generated
+		int sumPMarks;//process - sum of players marks
+		int sumCMarks;//process - sum of players marks
+		String replay; //input to play again
 		String message;//output
-		int userNumber;//input
-		int sumPMarks;
-		int sumCMarks;
-		int randNumber;
-		String replay; //added to play again
-		String winnerMessage;
+		String winnerMessage;//output
 		MorraPart2 myMorraPart2;
 
-			public MorraPart2Play() {
+			public MorraPart2Play() {//constructor - initialise the variables
 
-				side="";//input
-				message="";//output
-				userNumber=0;//input
+				side="";
+				message="";
+				userNumber=0;
 				sumPMarks=0;
 				sumCMarks=0;
 				randNumber=0;
-				replay=""; //added to play again
+				replay="";
 				winnerMessage="";
 				myMorraPart2=new MorraPart2();
 			}
@@ -42,7 +41,7 @@ public class MorraPart2Play{
 								myMorraPart2.setSide(side);
 								runGame();
 							}
-							else{// if the user doesn't enter a valid number the game repeates
+							else{// if the user doesn't enter a valid number the game repeats
 								JOptionPane.showMessageDialog(null, "Invalid value, start again.");
 							}
 					}
@@ -54,19 +53,19 @@ public class MorraPart2Play{
 				JOptionPane.showMessageDialog(null, winnerMessage);
 				replay=JOptionPane.showInputDialog("Do you want to play again?");
 				myMorraPart2.setReplay(replay);
-				if(replay.equalsIgnoreCase("yes")){
+				if(replay.equalsIgnoreCase("yes")){//resets the scores and replays the game if user chooses to play again
 					side="";//input
 					message="";//output
 					userNumber=0;//input
 					sumPMarks=0;
 					sumCMarks=0;
 					randNumber=0;
-					replay=""; //added to play again
+					replay=""; 
 					winnerMessage="";
 					myMorraPart2.reset();
 					return 0; // replays the game
 				}else{
-					myMorraPart2.finalHist();
+					myMorraPart2.finalHist();//show history of games if user rejects
 					JOptionPane.showMessageDialog(null,"Thank you!");
 					return 1;
 				}
@@ -76,7 +75,7 @@ public class MorraPart2Play{
 			public void runGame(){
 				try{
 				userNumber=Integer.parseInt(JOptionPane.showInputDialog(null, "Please, choose between 1 to 10: "));
-					if(userNumber>=1 && userNumber<=10){
+					if(userNumber>=1 && userNumber<=10){//ensure number is between 1 and 10
 						myMorraPart2.setUserNumber(userNumber);
 
 						//process
@@ -91,11 +90,11 @@ public class MorraPart2Play{
 						JOptionPane.showMessageDialog(null,"The computer chose: "+randNumber+"\n\nThis round result:"+"\nPlayer: "+sumPMarks+"\nComputer: "+sumCMarks); //have to show what the computer chose
 						}
 					else{
-						JOptionPane.showMessageDialog(null, "Ivalid value, start again.");
+						JOptionPane.showMessageDialog(null, "Invalid value, start again.");
 						runGame();
 					}
 				}
-				catch(NumberFormatException e){
+				catch(NumberFormatException e){//catches strings when it should be an int input
 					JOptionPane.showMessageDialog(null, "Invalid value, start again.");
 					runGame();
 				}
